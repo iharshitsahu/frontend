353c56299ad28353e3f8b9463d31108f8e9b631d
iharshitsahu <harshitmtr705@gmail.com>
2024-03-01 20:41:57 +0530

    React

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h2>Login Page</h2>
+        <button className='my-btn'>submit login</button>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Link href="/login">login</Link><br />
+      <Link href="/signup">signip</Link><br />
+      <Link href="/contact">contact</Link><br />
+      <Link href="/about">about</Link><br />
+
+      <h1 style={{color:'red', fontSize:'8rem'}}>My home page</h1>
+      <input type="text" /><br />
+      <button className='my-btn'>Submit</button>
+    </div>
+  )
+}
+
+
+export default Home;
